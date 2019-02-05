c40487e1efc40bd6d0083b4e2e6bf837d2d34017
Brett Israelsen <brett.israelsen@gmail.com>
2019-02-05 10:23:57 -0700

    added more detail to juliarc.jl

--- juliarc.jl
+++ juliarc.jl
@@ -1,2 +1,21 @@
-# addes current folder to the julia path so we can load the local modules
+##########
+## instructions for global `.juliarc.jl`
+##########
+
+# it is necessary to include the following in the global `.juliarc.jl`
+# it makes julia search the current directory for a local `juliarc.jl` file
+
+#  if pwd() != ENV["HOME"]
+    #  local_rc = joinpath(pwd(),"juliarc.jl")
+    #  if isfile(local_rc)
+        #  println("Loading Local juliarc.jl file....")
+        #  include(local_rc)
+    #  end
+#  end
+
+##########
+## local `juliarc.jl`
+##########
+
+# adds current folder to the julia path so we can load the local modules
 push!(LOAD_PATH,pwd())
